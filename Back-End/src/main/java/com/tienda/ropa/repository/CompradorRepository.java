e1783d0d47549f049b7c7c18723fb882159277e7
HatiUTP <celulencio50000@outlook.es>
2025-06-05 21:49:44 -0500

    Back-End JWT

--- Back-End/src/main/java/com/tienda/ropa/repository/CompradorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tienda.ropa.repository;
-
-import com.tienda.ropa.entity.Comprador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompradorRepository extends JpaRepository<Comprador, Long> {
-}
